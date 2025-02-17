89904d6d23d52b525f1acdcb321a3dcde20ab822
Aaron Flynn <114592687+Aaron2808@users.noreply.github.com>
2025-02-17 18:38:04 +0000

    Updated Documentation

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -3,8 +3,40 @@ import time
 import numpy as np
 from matplotlib import pyplot as plt
 
+def main():
 
-def gridSearch(file, populationSize, crossoverRate, mutationRate, generations,tournamentSize,elitism, drawPlot):
+    #GridSearch Parameters
+    populationSize = [200,600,1000]
+    crossoverRate = [0.6, 0.7, 0.8, 0.9]
+    mutationRate = [0.4,0.5,0.6,0.8]
+    generations = 100
+    elitism = 2
+    drawPlot =  False
+    iterations = 5
+
+    gridSearch("TSP Files/berlin25.txt", populationSize, crossoverRate, mutationRate, generations, elitism, drawPlot, iterations)
+
+    #geneticAlgorithm("TSP Files/pr1002.tsp", 3000, 1500, 0.9, 0.3,2,  True )
+
+
+
+def gridSearch(file, populationSize, crossoverRate, mutationRate, generations, elitism, drawPlot, iterations):
+
+    """
+        Iterates through given list of parameters and evaluates the average fitness across given iterations.
+        Prints out sorted list of the best parameters combinations and their average fitness.
+
+    :param file:
+    :param populationSize:
+    :param crossoverRate:
+    :param mutationRate:
+    :param generations:
+    :param elitism:
+    :param drawPlot:
+    :param iterations:
+    """
+
+
     results = []
 
     for size in populationSize:
@@ -13,8 +45,8 @@ def gridSearch(file, populationSize, crossoverRate, mutationRate, generations,to
                 print(f"Population: {size}, Crossover: {crossover}, Mutation: {mutation}")
 
                 fitnessScores = []
-                for _ in range(5):
-                    bestScore = geneticAlgorithm(file, size, generations, crossover, mutation,tournamentSize, elitism, drawPlot)
+                for _ in range(iterations):
+                    bestScore = geneticAlgorithm(file, size, generations, crossover, mutation, elitism, drawPlot)
                     fitnessScores.append(bestScore)
 
                 avgFitness = sum(fitnessScores) / len(fitnessScores)
@@ -27,7 +59,23 @@ def gridSearch(file, populationSize, crossoverRate, mutationRate, generations,to
         print(f"Population: {result[0]}, Crossover: {result[1]}, Mutation: {result[2]} -> Average Fitness: {result[3]:.4f}")
 
 
-def geneticAlgorithm(file, populationSize, generations, crossoverRate, mutationRate,tournamentSize, elitism, drawPlot):
+
+def geneticAlgorithm(file, populationSize, generations, crossoverRate, mutationRate, elitism, drawPlot):
+
+    """
+        Implements selection, crossover and mutation operations to create, explore and calculate fitness of solutions.
+        Repeats this over generations aiming to improve fitness each time
+        Prints our best fitness produced after each generation
+        Plots fitness over generations
+
+    :param file:
+    :param populationSize:
+    :param generations:
+    :param crossoverRate:
+    :param mutationRate:
+    :param elitism:
+    :param drawPlot:
+    """
     generationFitness = []
     startTime = time.time()
 
@@ -90,6 +138,12 @@ def geneticAlgorithm(file, populationSize, generations, crossoverRate, mutationR
 
 
 def readFile(file):
+
+    """
+        Takes in TSP File and parses its data
+    :param file:
+    :return: dimension, fitness
+    """
     infile = open(file, 'r')
 
     name = infile.readline().strip().split()[1]
@@ -126,6 +180,13 @@ def readFile(file):
     return dimension, coordinates
 
 def initialPopulation(totalLocations, populationSize):
+
+    """
+        Generates initial population of given size.
+    :param totalLocations:
+    :param populationSize:
+    :return: randomly initialized population
+    """
     randomRoutes = []
     for _ in range(populationSize):
         route = list(range(totalLocations))
@@ -135,22 +196,56 @@ def initialPopulation(totalLocations, populationSize):
 
 
 def calculateFitness(locations,route):
+
+    """
+        Calculates fitness of given route.
+    :param locations:
+    :param route:
+    :return: fitness
+    """
     fitness = 0
     for i in range(len(route)):
         fitness += distance(locations[route[i]], locations[route[(i+1) % len(route)]])
     return fitness
 
 def distance(location1, location2):
+    """
+        Calculates distance between two locations.
+    :param location1:
+    :param location2:
+    :return: distance
+    """
     return np.sqrt((location1[0] - location2[0])**2 + (location1[1] - location2[1])**2)
 
 
 def tournamentSelection(population, fitness, size):
+    """
+        Selects a parent from the population using tournament selection.
+
+        Tournament selection works by selecting a subset of individuals
+        from the population, comparing their fitness values, and returning the
+        individual with the best fitness.
+
+    :param population:
+    :param fitness:
+    :param size:
+    :return: Best route based on fitness value
+    """
     tournament = random.sample(list(zip(population, fitness)), size)
     tournament.sort(key=lambda x: x[1])
     return tournament[0][0]
 
 
 def orderCrossover(parent1, parent2):
+
+    """
+        Creates two offspring from two given parents using order crossover.
+        Order crossover randomly selects a segment from one parent and crosses it with the other parent
+
+    :param parent1:
+    :param parent2:
+    :return: child1, child2
+    """
     size = len(parent1)
     child1, child2 = [-1] * size, [-1] * size
 
@@ -173,6 +268,16 @@ def orderCrossover(parent1, parent2):
 
 
 def partiallyMappedCrossover(parent1, parent2):
+
+    """
+        Creates two offspring using partially mapped crossover.
+        It crosses segments of the two parents while maintaining relative order
+
+
+    :param parent1:
+    :param parent2:
+    :return: child1, child2
+    """
     point1 = random.randrange(0, len(parent1) - 1)
     point2 = random.randrange(point1 + 1, len(parent1))
 
@@ -199,6 +304,13 @@ def partiallyMappedCrossover(parent1, parent2):
 
 
 def swapMutation(parent):
+
+    """
+        Applies swap mutation to a parent
+        Selected two random points in the solution and swaps them
+    :param parent:
+    :return: mutated parent
+    """
     mutation = parent[:]
     point1, point2 = random.sample(range(len(mutation)), 2)
 
@@ -209,6 +321,13 @@ def swapMutation(parent):
     return mutation
 
 def inversionMutation(parent):
+
+    """
+        Applies inversion mutation to a parent
+        Selected two random points in the solution and inverses the segment between them
+    :param parent:
+    :return: mutated parent
+    """
     mutation = parent[:]
     point1, point2 = random.sample(range(len(mutation)), 2)
 
@@ -216,16 +335,5 @@ def inversionMutation(parent):
 
     return mutation
 
-
-
-populationSize = [200,600,1000]
-crossoverRate = [0.6, 0.7, 0.8, 0.9]
-mutationRate = [0.4,0.5,0.6,0.8]
-tournamentSize = 30
-generations = 100
-elitism = 4
-drawPlot =  False
-
-#gridSearch("TSP Files/berlin25.txt", populationSize, crossoverRate, mutationRate, generations,tournamentSize, elitism, drawPlot)
-
-geneticAlgorithm("TSP Files/pr1002.tsp", 3000, 1500, 0.9, 0.3, 5,2,  True )
+if __name__ == "__main__":
+    main()
